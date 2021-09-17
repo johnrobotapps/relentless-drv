81daae96dd5246b07109476d17cbd631e0bdbbbe
johnrobotapps <starrysasquatch@gmail.com>
2021-09-17 22:21:27 +0000

    init file updates

--- relentlessdrv/__init__.py
+++ relentlessdrv/__init__.py
@@ -1,10 +1,5 @@
 
 
-#from ._version import get_versions
-#__version__ = get_versions()['version']
-#version = __version__
-#del get_versions
-
 from .datastructures import *
 from .database import *
 
--- relentlessdrv/database/__init__.py
+++ relentlessdrv/database/__init__.py
@@ -1,5 +1,5 @@
 
 
-from . import *
+from .mongodb import *
 
 
