7e4d6238064c1362cae8f957fe4fbb8f6ee8e808
johnrobotapps <starrysasquatch@gmail.com>
2021-09-20 19:19:40 -0500

    dbmeta metaclass created

---
+++ relentlessdrv/database/dbmeta.py
@@ -0,0 +1,27 @@
+
+
+from .mongodb import MongoDB, MongoClient
+
+
+
+class DBMeta(type):
+
+
+    def __init__(cls, *args, **kwargs):
+
+        cls._database = None
+
+
+    @property
+    def database(cls):
+
+        return cls._database
+
+
+    @database.setter
+    def database(cls, mongodb):
+
+        if cls._database is None:
+            if type(mongodb) is MongoClient:
+                cls._database = mongodb
+
